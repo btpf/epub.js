5dbabcc30ac3b4e1a68d3e6de4968a6bad07daf8
btpf <bretpapkoff@Gmail.com>
2023-09-05 21:18:34 -0400

    Added openJSON Method

--- src/book.js
+++ src/book.js
@@ -337,7 +337,16 @@ class Book {
 				return this.unpack(this.packaging);
 			});
 	}
-
+	/**
+	* Load manifest JSON object
+	* @param  {Object} json
+	* @return {Promise}
+	*/
+	openJSON(json) {
+		this.packaging = new Packaging();
+		this.packaging.load(json);
+		return this.unpack(this.packaging);
+	}
 	/**
 	 * Load a resource from the Book
 	 * @param  {string} path path to the resource to load
